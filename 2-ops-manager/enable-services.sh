36e000152a90621d6c48a4b089ad8828efae65c7
shinyay <shinya.com@gmail.com>
2019-05-29 15:54:59 +0900

    Add a script to enable services for pcf

---
+++ 2-ops-manager/enable-services.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+gcloud services enable compute.googleapis.com && \
+gcloud services enable iam.googleapis.com && \
+gcloud services enable cloudresourcemanager.googleapis.com && \
+gcloud services enable dns.googleapis.com && \
+gcloud services enable sqladmin.googleapis.com
