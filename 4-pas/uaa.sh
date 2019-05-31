339441ef469f6798354461693905c400e80dbfb6
shinyay <shinya.com@gmail.com>
2019-05-31 04:26:22 +0000

    Add UAA configuration

---
+++ 4-pas/uaa.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "[SAML Service Provider Credentials - Certificate PEM]"
+cat ../2-ops-manager/mypcf.syanagihara.cf.cert
+
+echo "[SAML Service Provider Credentials - Private Key PEM]"
+cat ../2-ops-manager/mypcf.syanagihara.cf.key
