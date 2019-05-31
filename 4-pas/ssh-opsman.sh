aeb1f5a4d0c8612d1eb135d55ba21255555ff349
shinyay <shinya.com@gmail.com>
2019-05-31 15:53:27 +0900

    Modify ops manager host name

--- 4-pas/ssh-opsman.sh
+++ 4-pas/ssh-opsman.sh
@@ -3,5 +3,5 @@
 echo "[Ops Manager Info]"
 gcloud compute instances list|grep ops-manager
 
-echo "[SSH to Ops Manager]"
-gcloud compute ssh ubuntu@pcf-ops-manager --zone asia-northeast1-a --force-key-file-overwrite --strict-host-key-checking=no
+echo "[SSH to Ops Manager(mypcf-ops-manager --zone asia-northeast1-a)]"
+gcloud compute ssh ubuntu@mypcf-ops-manager --zone asia-northeast1-a --force-key-file-overwrite --strict-host-key-checking=no
