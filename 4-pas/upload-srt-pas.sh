80550bca8f9bd5401820e22656c5fddb7a9b2f3a
shinyay <shinya.com@gmail.com>
2019-05-31 03:19:24 +0000

    Add staging process

--- 4-pas/upload-srt-pas.sh
+++ 4-pas/upload-srt-pas.sh
@@ -3,3 +3,11 @@
 TARGET_FILENAME=srt-2.5.4-build.5.pivotal
 
 om --username admin --password ${PCF_OPSMAN_ADMIN_PASSWD} --target https://${PCF_OPSMAN_FQDN} --skip-ssl-validation upload-product --product ${TARGET_FILENAME}
+om \
+	--username admin \
+	--password ${PCF_OPSMAN_ADMIN_PASSWD} \
+	--target https://${PCF_OPSMAN_FQDN} \
+	--skip-ssl-validation \
+	stage-product \
+	--product-name cf \
+	--product-version 2.5.4
