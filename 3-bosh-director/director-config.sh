64feb04c8d2c17821ffa4b9da6c85b4459e58753
shinyay <shinya.com@gmail.com>
2019-05-31 00:45:47 +0000

    Add info for direcor config

---
+++ 3-bosh-director/director-config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "[NTP Servers]"
+echo "169.254.169.254"
+
+echo "[Enable VM Resurrector Plugin]"
+echo true
+
+echo "[Enable Post Deploy Scripts]"
+echo true
+
+echo "[Enable bosh deploy retries]"
+echo true
