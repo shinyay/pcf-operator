6a8eeb7d0fb88b721d3b205fadc4e2af4c54f15b
shinyay <shinya.com@gmail.com>
2019-05-31 05:46:08 +0000

    errand.sh

---
+++ 4-pas/errand.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "[Errand]"
+echo "Following items are OFF:
+- Notifications Errand
+- Notifications UI Errand
+- App Autoscaler Errand
+- App Autoscaler Smoke Test Errand
+- NFS Broker Errand"
+
