7965faa23b66b0a8f29350aca740b49fd221e3a5
shinyay <shinya.com@gmail.com>
2019-06-05 12:50:02 +0000

    Modify sleeping time between upload and stage

--- 6-mysql/upload-mysql.sh
+++ 6-mysql/upload-mysql.sh
@@ -3,6 +3,7 @@
 TARGET_FILENAME=pivotal-mysql-2.5.4-build.51.pivotal
 
 om --username admin --password ${PCF_OPSMAN_ADMIN_PASSWD} --target https://${PCF_OPSMAN_FQDN} --skip-ssl-validation upload-product --product ${TARGET_FILENAME}
+sleep 5
 om \
 	--username admin \
 	--password ${PCF_OPSMAN_ADMIN_PASSWD} \
